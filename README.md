3f03b1c1dc929d6eca308d68d42d7394b11e928f
My Name <raymondliusmailbox@gmail.com>
2025-06-08 15:40:20 -0400

    Revamp

---
+++ README.md
@@ -0,0 +1,1 @@
+View Site by opening index.html or view at https://htmlpreview.github.io/?https://github.com/Raymond-Liu0503/Personal-Website/blob/main/index.html
--- scripts.js
+++ scripts.js
@@ -1,90 +1,42 @@
-
-// Script for scroll animations
-
-const hiddenElement = document.querySelectorAll(".hidden");
+// --- Script for scroll-reveal animations ---
+const hiddenElements = document.querySelectorAll(".hidden");
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("visible");
-        }else{
-            entry.target.classList.remove("visible");
-        }
-    });
-});
-
-hiddenElement.forEach((element) => {
-    observer.observe(element);
-});
-
-// const insetElement = document.querySelectorAll(".contact-card");
-// const observer2 = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("inset-box");
-//         }else{
-//             entry.target.classList.remove("inset-box");
-//         }
-//     });
-// });
-
-// insetElement.forEach((element) => {
-//     observer2.observe(element);
-// });
-
-//Script for rotating cards
-
-let constrain = 175;
-let mouseOverContainer = document.querySelectorAll(".swervo-container");
-let ex1Layer = document.querySelectorAll(".swervo");
-let isRotated = false;
-
-function transforms(x, y, el) {
-  let box = el.getBoundingClientRect();
-  let calcX = -(y - box.y - (box.height / 2)) / constrain;
-  let calcY = (x - box.x - (box.width / 2)) / constrain;
-  
-  return "perspective(100px) "
-    + "   rotateX("+ calcX +"deg) "
-    + "   rotateY("+ calcY +"deg) ";
-};
-
- function transformElement(el, xyEl) {
-    isRotated = true;
-    el.style.transform  = transforms.apply(null, xyEl);
-
-}
-
-function resetRotation() {
-    for(let i = 0; i < ex1Layer.length; i++){ //Really inefficient, but it works
-        ex1Layer[i].style.transform = "none"; // Reset the rotation
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
     }
-    isRotated = false;
-  }
+  });
+});
 
-for(let i = 0; i < mouseOverContainer.length; i++){
-    mouseOverContainer[i].onmousemove = function(e) {
-    let xy = [e.clientX, e.clientY];
-    let position = xy.concat([ex1Layer[i]]);
-    
+hiddenElements.forEach((element) => observer.observe(element));
 
-    window.requestAnimationFrame(function(){
-        if (isRotated){
-            // console.log(ex1Layer[element]);
-            transformElement(ex1Layer[i], position);
-        }else{
-            resetRotation(ex1Layer[i]);
-        }
-        
+
+// --- Script for 3D rotating cards on hover ---
+const constrain = 175; // Adjusts the "sensitivity" of the rotation
+const swervoContainers = document.querySelectorAll(".swervo-container");
+
+swervoContainers.forEach(container => {
+  const layer = container.querySelector(".swervo");
+
+  container.addEventListener("mousemove", (e) => {
+    const box = layer.getBoundingClientRect();
+    const calcX = -(e.clientY - box.y - box.height / 2) / constrain;
+    const calcY = (e.clientX - box.x - box.width / 2) / constrain;
+
+    const transform = `perspective(100px) rotateX(${calcX}deg) rotateY(${calcY}deg)`;
+
+    // Use requestAnimationFrame for smoother animation
+    window.requestAnimationFrame(() => {
+      layer.style.transform = transform;
     });
-    };
-};
+  });
 
-function addRotation(){
-    isRotated = true;
-}
-
-ex1Layer.forEach((element) =>{
-    console.log(element);
-    element.addEventListener('mouseenter', addRotation);
-    element.addEventListener('mouseleave', resetRotation);
+  container.addEventListener("mouseleave", () => {
+    // Reset the transform when the mouse leaves the container
+    window.requestAnimationFrame(() => {
+        layer.style.transform = "perspective(100px) rotateX(0) rotateY(0)";
+    });
+  });
 });
